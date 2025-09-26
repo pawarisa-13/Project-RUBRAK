a927f3a0e195001a9e83157bc7e183f409723be1
pawarisa-13 <pawarisa.see@kkumail.com>
2025-09-26 22:16:15 +0700

    Update AdminUserSeeder.php

--- RUBRAKlaravel/database/seeders/AdminUserSeeder.php
+++ RUBRAKlaravel/database/seeders/AdminUserSeeder.php
@@ -14,11 +14,30 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
+        //Admin 1
         User::updateOrCreate(
             ['email' => 'admin@example.com'],
             [
                 'name' => 'Super Admin',
                 'password' => Hash::make('password123'), // เปลี่ยนทีหลัง!
+                'role' => 'admin',
+            ]
+        );
+        //Admin 2
+        User::updateOrCreate(
+            ['email' => 'admin2@example.com'],
+            [
+                'name' => 'Second Admin',
+                'password' => Hash::make('password123'),
+                'role' => 'admin',
+            ]
+        );
+        //Admin 3
+        User::updateOrCreate(
+            ['email' => 'admin3@example.com'],
+            [
+                'name' => 'Second Admin',
+                'password' => Hash::make('password123'),
                 'role' => 'admin',
             ]
         );
