4c95f64e9b3e7bad0bf2d397d9c87adba80e93cd
pawarisa-13 <pawarisa.see@kkumail.com>
2025-09-26 21:12:11 +0700

    seed admin user

---
+++ RUBRAKlaravel/database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password123'), // เปลี่ยนทีหลัง!
+                'role' => 'admin',
+            ]
+        );
+    }
+}
+
+
