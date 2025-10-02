094e56cae41aa024befa567633198eb5b3f3e39c
gumaichopcode <kamolponcode@gmail.com>
2025-10-02 23:34:57 +0700

    Update contact.blade.php

--- RUBRAKlaravel/resources/views/contact.blade.php
+++ RUBRAKlaravel/resources/views/contact.blade.php
@@ -37,7 +37,44 @@
         <div>
             <img src="{{ asset('Pic-rubrak/homepic.jpg') }}" alt="contactpic" width="635" height="741" style="float: right; margin-left: 10px;">
         </div>
-        <h1>Contact Page</h1>
+        <div class="contact-context">
+            <h1>Contact Page</h1><br>
+            <p>Every dog deserves a second chance. They are waiting patiently for someone to open their heart and home.</p>
+        </div>
+        <div class="add-contact">
+            <h2>Address</h2>
+            <p>45 Maplewood Avenue Greenhill, NY 12845 United States</p>
+            <p>18 Rosewood Lane Riverdale, London SW3 7TP United Kingdom</p>
+        </div>
+        <div class="gen-contect">
+            <h2>General inquiries</h2>
+            <p>info@sunnytechsolutions.com <br>Phone: +1 (408) 555-9274</p>
+            <br>
+            <p>emily.johnson@examplemail.com <br>Phone: +1 (212) 555-3489</p>
+        </div>
+        <div class="inter-contact">
+            <h2></h2>
+            <p>careers@oceanbreezeinc.com</p>
+        </div>
+        <div class="social">
+            <h2>Follow us</h2>
+            <div class="media">
+                <img src="" alt="">
+                <p>TW</p>
+            </div>
+            <div class="media">
+                <img src="" alt="">
+                <p>FB</p>
+            </div>
+            <div class="media">
+                <img src="" alt="">
+                <p>IG</p>
+            </div>
+            <div class="media">
+                <img src="" alt="">
+                <p>TT</p>
+            </div>
+        </div>
     </main>
 </body>
 </html>
