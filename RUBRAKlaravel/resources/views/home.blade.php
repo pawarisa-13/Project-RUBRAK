08279d414040bde41953d07efece9cdca5692218
gumaichopcode <kamolponcode@gmail.com>
2025-10-02 20:31:12 +0700

    home

--- RUBRAKlaravel/resources/views/home.blade.php
+++ RUBRAKlaravel/resources/views/home.blade.php
@@ -4,101 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/header.css')}}">
     <title>Rubrak</title>
 </head>
 <body>
     <header>
         <div class="logo">
-            <img src="{{ asset('Pic-rubrak/LogoRubRak.png.PNG') }}"  width="36" alt="imglogo">
-            <h4>Rubrak</h4>
+            <img src="{{ asset('Pic-rubrak/LogoRubRak.png.PNG') }}"  alt="imglogo">
+            <p>Rubrak</p>
         </div>
-        <div class="nav">
-            
+        <div class="nav">     
         <ul>
             <li class="menu"><a href="{{route ('home')}}">Home</a></li>
             <li class="menu"><a href="{{route ('pets.index')}}">Pet</a></li>
             <li class="menu"><a href="{{route ('donate')}}">Donate</a></li>
             <li class="menu"><a href="{{route ('contact')}}">Contact Us</a></li>
-            <li class="menu"><a href="{{route ('profile')}}">Profile</a></li>
-        </ul>
+            </ul>
+        </div>
+        <div class="btn">
+            @auth
+            {{-- ถ้าล็อกอินแล้ว --}}
+                <a href="{{route('profile')}}"><button class="btn-header">Hello!, {{ Auth::user()->name }}</button></a>
+            @else
+            {{-- ถ้ายังไม่ล็อกอิน --}}
+            <a href="{{route('login')}}"><button class="btn-header">Sign In</button></a>
+            <a href="{{route('register')}}"><button class="btn-header">Sign Up</button></a>
+            @endauth
         </div>
     </header>
-    <div class="header-stripe"></div>
 
 
     <main>
         <div>
-            <img src="" alt="">
+            <img src="{{ asset('Pic-rubrak/homepic.jpg') }}" alt="homepic" width="635" height="741" style="float: left; margin-right: 10px;">
         </div>
-        <img src="" alt="">
-        <div>
-            <h2>Welcome to “Rub Rak”</h2> <a href="{{route ('request.form')}}">Adopt Now</a>
+        <div class="home-content">
+            <h1>Welcome to “Rub Rak”</h1> 
             <br>
-            <p>Every animal deserves a safe, loving home.
+            <div>
+                <p>Every animal deserves a safe, loving home.
             Here, you’ll find loyal companions who are ready to bring joy, and warmth into your life.
-            When you adopt, you’re not just giving an animal a home-you’re opening your heart to a lifetime of unconditional love </p>
-            <br><br>
-            <p>i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci i love comsci
-</p>
+            When you adopt, you’re not just giving an animal a home-you’re 
+            opening your heart to a lifetime of unconditional love. </p>
+            </div>
+            <br>
+            <a href="{{route ('pet.filter')}}"><button class="adopt-button">Adopt Now</button></a>
         </div>
     </main>
 </body>
--- RUBRAKlaravel/routes/web.php
+++ RUBRAKlaravel/routes/web.php
@@ -41,7 +41,7 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-         return view('dashboard');
+         return redirect('/');
     })->name('dashboard');
 
 
